6ae300c6b8b992f7fcdee35d47848158915ee343
vishvak8 <156602526+vishvak8@users.noreply.github.com>
2025-01-15 06:07:42 +0530

    Update fetch_transcript.py

--- fetch_transcript.py
+++ fetch_transcript.py
@@ -7,7 +7,7 @@ import requests
 import re
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5174"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 # Nhost configuration
 NHOST_GRAPHQL_URL = "https://pcpbxvkqnfgbyqbsydgy.hasura.ap-south-1.nhost.run/v1/graphql"
